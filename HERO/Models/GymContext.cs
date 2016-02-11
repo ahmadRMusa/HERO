7b12156523d952d30ffe910716f7afbd1227e1ca
Alex Morask <Alex Morask>
2016-02-11 16:35:05 -0600

    Reverted to pre-class version

--- HERO/Models/GymContext.cs
+++ HERO/Models/GymContext.cs
@@ -26,7 +26,6 @@ namespace HERO.Models
         public virtual DbSet<Athlete> Athletes { get; set; }
         public virtual DbSet<Subscription> Subscriptions { get; set; }
         public virtual DbSet<AthleteSignupKey> AthleteSignupKeys { get; set; }
-        public virtual DbSet<Class> Classes { get; set; }
     }
 
     //public class MyEntity
