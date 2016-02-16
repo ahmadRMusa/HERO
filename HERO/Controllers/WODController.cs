1aaa4201040571b0fa65c9e2825d526d3b14d4ca
Alex Morask <Alex Morask>
2016-02-16 12:14:18 -0600

    Working on adding existing wods to classes

--- HERO/Controllers/WODController.cs
+++ HERO/Controllers/WODController.cs
@@ -55,6 +55,11 @@ namespace HERO.Controllers
             }
         }
 
+        public ActionResult AddToClass(int classId)
+        {
+            throw new NotImplementedException();
+        }
+
         // POST: WOD/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
