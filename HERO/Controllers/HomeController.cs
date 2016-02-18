7227ae2e80f040221c2c3ab2aaead520460ed52a
Alex Morask <Alex Morask>
2016-02-18 11:17:06 -0600

    Added Reminders for WOD Performance

--- HERO/Controllers/HomeController.cs
+++ HERO/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HERO.Models;
+using HERO.Models.Objects;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,14 +12,42 @@ namespace HERO.Controllers
     [Authorize]
     public class HomeController : Controller
     {
+        private GymContext db;
+
+        public HomeController(GymContext context)
+        {
+            db = context;
+        }
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult TestCalendar()
+        public ActionResult Reminders()
         {
-            return View();
+            string userId = HttpContext.User.Identity.GetUserId();
+            List<Reminder> reminders = GetMissingPerformanceReminders(userId);
+            return View(reminders);
+        }
+
+        public List<Reminder> GetMissingPerformanceReminders(string userId)
+        {
+            int athleteId = db.Athletes.Select(a => new { Id = a.Id, appId = a.ApplicationUserId }).Single(c => c.appId.Equals(userId)).Id;
+            var classData = db.Classes.AsNoTracking().Select(c => new { Id = c.Id, Type = c.Type, Date = c.Time, Performances = c.Performances, Attendance = c.Attendance })
+                .Where(p => p.Attendance.Select(a => a.Id).Contains(athleteId) && p.Date.Value <= DateTime.Now && !p.Performances.Select(d => d.Athlete.Id).Contains(athleteId)).Select(a => new { Id = a.Id, Type = a.Type, Date = a.Date } );
+            List<Reminder> reminders = new List<Reminder>();
+            foreach(var item in classData)
+            {
+                reminders.Add(new Reminder
+                {
+                    Type = ReminderType.RecordWod,
+                    ClickId = item.Id,
+                    Date = item.Date,
+                    Message = String.Format("Remember to record your performance for {0} on {1}.", item.Type, item.Date.Value.ToString("MM/dd/yyyy"))
+                });
+            };
+            return reminders;
         }
     }
 }
