96d37bc16a595c8df0380c87667bda3dd5788179
Alex Morask <Alex Morask>
2016-02-21 18:44:58 -0600

    successfully implemented email reminders for weekly classes

--- HERO/Global.asax.cs
+++ HERO/Global.asax.cs
@@ -19,7 +19,7 @@ namespace HERO
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            // JobScheduler.Start();
+            JobScheduler.Start();
         }
     }
 }
--- HERO/Services/ReminderSender.cs
+++ HERO/Services/ReminderSender.cs
@@ -21,7 +21,9 @@ namespace HERO.Services
             {
                 TimeSpan fullDay = new TimeSpan(1, 0, 0, 0, 0);
 
-                List<Class> classesToRemind = athlete.Reminders.Reminders.Where(c => c.Time - DateTime.Now <= fullDay).ToList();
+                TimeSpan noTime = new TimeSpan(0, 0, 0);
+
+                List<Class> classesToRemind = athlete.Reminders.Reminders.Where(c => c.Time - DateTime.Now <= fullDay && c.Time - DateTime.Now >= noTime).ToList();
 
                 foreach(var cls in classesToRemind)
                 {
@@ -39,7 +41,7 @@ namespace HERO.Services
             string subject = String.Format("Don't Forget to Signup for {0}!", cls.Type);
             string greeting = String.Format("<h1>Hey there!</h1>");
             string body = String.Format("<p>You still haven't signed up for your {0} class on {1}!</p>", cls.Type, cls.Time.Value.ToString("MM/dd/yyyy"));
-            string link = String.Format(@"<p><a href = ""http://localhost:11700/Account/Classes/Details?Id={0}"">Click here to signup!</a></p>", cls.Id);
+            string link = String.Format(@"<p><a href = ""http://localhost:11700/Classes/Details?Id={0}"">Click here to signup!</a></p>", cls.Id);
 
             string message = greeting + body + link;
 
